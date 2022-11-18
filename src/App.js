c3a14cb3b8c996491b12a2772110392b9a7f23ab
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-18 21:30:41 +0300

    Login component created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+//components
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello Disney!</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Login(props) {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
