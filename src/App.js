a8c71774c2a146778f4f97167e60e68279b43d7e
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-19 17:47:31 +0300

    Navbar created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 //components
 import Login from './components/Login';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
         </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header(props) {
+  return (
+    <Nav>Header</Nav>
+  )
+}
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #040714;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+export default Header
