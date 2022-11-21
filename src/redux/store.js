ee7515b2a717ca5a31a1ba68072b3131183fa367
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-21 21:26:37 +0300

    userSlice imported.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import userSlice from "../features/users/userSlice";
 
-export default configureStore({
-    reducer: {},
+export const store = configureStore({
+    reducer: {
+        user: userSlice,
+    },
     middleware: getDefaultMiddleware({
         serializableCheck: false,
     }),
