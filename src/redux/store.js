070d9cc9eceda1d33c3cba04f1b779c5c1dcc528
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-23 14:51:25 +0300

    Updated.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore, /*getDefaultMiddleware*/ } from "@reduxjs/toolkit";
 import userSlice from "../features/users/userSlice";
 
 export const store = configureStore({
