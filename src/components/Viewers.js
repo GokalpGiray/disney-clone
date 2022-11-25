de33fcebd4794446908963004fc6e911e3c944a2
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-26 01:06:04 +0300

    updated.

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -89,7 +89,7 @@ const Wrap = styled.div`
     };
 
     &:hover {
-        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
         transform: scale(1.05);
         border-color: rgba(249, 249, 249, 0.8);
 
