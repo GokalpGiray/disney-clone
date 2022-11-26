63af40461d9a4cab944f7659efa3f9bdd7ba90b8
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-26 21:18:47 +0300

    useEffect added.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/users/userSlice';
+import db from "../firebase"
+import { onSnapshot, collection, query } from "firebase/firestore";
 
 //components
 import ImageSlider from './ImageSlider';
@@ -10,6 +15,50 @@ import Trending from './Trending';
 import Viewers from './Viewers';
 
 function Home() {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+        const q = query(collection(db, "movies"));
+
+        onSnapshot(q, (querySnapshot) => {
+            querySnapshot.docs.map((doc) => {
+
+        // db.collection("movies").onSnapshot((snapshot) => {
+        //     snapshot.docs.map((doc) => { 
+                switch (doc.data().type) {
+                    case "recommend":
+                        recommends = [...recommends, { id: doc.id, ...doc.data() }]
+                        break;
+                    case "new":
+                        newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }]
+                        break;
+                    case "original":
+                        originals = [...originals, { id: doc.id, ...doc.data() }]
+                        break;
+                    case "trending":
+                        trending = [...trending, { id: doc.id, ...doc.data() }]
+                        break;
+                }
+            });
+        });
+
+        dispatch(
+            setMovies({
+                recommend: recommends,
+                newDisney: newDisneys,
+                original: originals,
+                trending: trending,
+            }
+        ));
+
+    }, [userName]);
+
     return (
         <Container>
             <ImageSlider />
