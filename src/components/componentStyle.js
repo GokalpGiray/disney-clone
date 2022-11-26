0e9f6601b508315f1e8f87d1a44960cd8c23c1af
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-26 22:15:25 +0300

    Movies function created and exported.

--- src/components/componentStyle.js
+++ src/components/componentStyle.js
@@ -1,10 +1,30 @@
 import styled from "styled-components";
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function Movies({movies}) {
+    return (
+        <Content>
+            {
+                movies &&
+                movies.map((movie, key) => (
+                    <Wrap key={key}>
+                        {movie.id}
+                        <Link to={`/detail/${movie.id}`} >
+                            <img src={movie.cardImg} alt={movie.title} />
+                        </Link>
+                    </Wrap>
+                ))
+            }
+        </Content>
+    )
+}
 
 export const Container = styled.div`
     padding: 0 0 26px;
 `;
 
-export const Content = styled.div`
+const Content = styled.div`
     display: grid;
     grid-gap: 25px;
     gap: 25px;
@@ -15,7 +35,7 @@ export const Content = styled.div`
     };
 `;
 
-export const Wrap = styled.div`
+const Wrap = styled.div`
     padding-top: 56.25%;
     border-radius: 10px;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
@@ -44,3 +64,5 @@ export const Wrap = styled.div`
         border-color: rgba(249, 249, 249, 0.8)
     }
 `;
+
+export default Movies
