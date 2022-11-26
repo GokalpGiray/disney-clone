34347c93c2fd165dcce4b71684ee7628a0c8186a
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-26 16:07:29 +0300

    movieSlice imported.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore, /*getDefaultMiddleware*/ } from "@reduxjs/toolkit";
+import movieSlice from "../features/movie/movieSlice";
 import userSlice from "../features/users/userSlice";
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
+        movie: movieSlice,  
     },
     // middleware: getDefaultMiddleware({
     //     serializableCheck: false,
